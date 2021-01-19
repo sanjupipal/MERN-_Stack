66501834ba0850f79b2cfb877a99c0e809d8beab
Sanju <sanjupipal@gmail.com>
2021-01-19 11:11:23 +0530

    most popular links ui

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,8 @@ import Layout from '../components/layout';
 import axios from 'axios';
 import {API} from '../config'
 import Link from 'next/link';
-
+import {useEffect, useState} from 'react'
+import moment from 'moment';
 const  Home = ({categories}) => {
     const listCategories = () => categories.map((c,i)=>(
         <Link href={`/links/${c.slug}`}>
@@ -21,6 +22,46 @@ const  Home = ({categories}) => {
         </Link>
     ))
 
+    const [popular , setPopular] = useState([])
+    useEffect(()=>{
+        loadPopular()
+    },[])
+
+    const loadPopular = async ()=>{
+        const response = await axios.get(`${API}/link/popular`)
+        setPopular(response.data)
+    }
+    const handleClick = async (linkId ) =>{
+        const response = await axios.put(`${API}/click-count`, {linkId})
+        loadPopular()
+    }
+
+    const listOfLinks = () =>(
+        popular.map((l,i)=>(
+            <div key={i} className="row alert alert-secondary p-2">
+                <div className="col-md-8" onClick={()=>handleClick(l._id)}>
+                    <a href={l.url} target="_blank">
+                        <h5 className="pt-2">{l.title}</h5>
+                        <h6 className="pt-2 text-danger" style={{fontSize:'12px'}}>
+                            {l.url}
+                        </h6>
+                    </a>
+                </div>
+                <div className="col-md-4 pt-2">
+                    <span className="pull-right">{moment(l.createdAt).fromNow()} by {l.postedBy.name}</span>
+                    <br/>
+                    <span className="badge text-secondary pull-right">{l.clicks} Clicks</span>
+                </div>
+                <div className="col-md-12">
+                    <span className="badge text-danger">{l.type}</span>{'/'}
+                    <span className="badge text-success">{l.medium}</span>
+                     {l.categories.map((c,i)=>{
+                         <span key={i} className="badge text-success">{c.name}</span>
+                    })}
+                </div>
+            </div>
+        ))
+    )
     return(
         <Layout>
             <div className="row">
@@ -33,6 +74,10 @@ const  Home = ({categories}) => {
             </div>
             <div className="row">
                 {listCategories()}
+            </div>
+            <div className="row pt-5">
+                <h2 className="font-weight-bold pb-3">Trending</h2>
+                <div className="col-md-12 overflow-hidden">{listOfLinks()}</div>
             </div>
         </Layout>
     )
--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout';
 import axios from 'axios'
 import { API } from '../../config'
 import Link from 'next/link'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import renderHTML from 'react-render-html'
 import moment from 'moment'
 import InfiniteScroll from 'react-infinite-scroller'
@@ -13,10 +13,50 @@ const Links = ({query,category,links,totalLinks,linksLimit,linkSkip}) =>{
     const [limit, setLimit] = useState(linksLimit)
     const [skip , setSkip] = useState(0)
     const [size, setSize] = useState(totalLinks)
+    const [popular , setPopular] = useState([])
+
+
+    useEffect(()=>{
+        loadPopular()
+    },[])
+
+    const loadPopular = async ()=>{
+        const response = await axios.get(`${API}/link/popular/${category.slug}`)
+        setPopular(response.data)
+    }
+    const listOfPopularLinks = () =>(
+        popular.map((l,i)=>(
+            <div key={i} className="row alert alert-secondary p-2">
+                <div className="col-md-8" onClick={()=>handleClick(l._id)}>
+                    <a href={l.url} target="_blank">
+                        <h5 className="pt-2">{l.title}</h5>
+                        <h6 className="pt-2 text-danger" style={{fontSize:'12px'}}>
+                            {l.url}
+                        </h6>
+                    </a>
+                </div>
+                <div className="col-md-4 pt-2">
+                    <span className="pull-right">{moment(l.createdAt).fromNow()} by {l.postedBy.name}</span>
+                    <br/>
+                    <span className="badge text-secondary pull-right">{l.clicks} Clicks</span>
+                </div>
+                <div className="col-md-12">
+                    <span className="badge text-danger">{l.type}</span>{'/'}
+                    <span className="badge text-success">{l.medium}</span>
+                     {l.categories.map((c,i)=>{
+                         <span key={i} className="badge text-success">{c.name}</span>
+                    })}
+                </div>
+            </div>
+        ))
+    )
+
+
 
     const handleClick = async (linkId ) =>{
         const response = await axios.put(`${API}/click-count`, {linkId})
         loadUpdatedLinks()
+        loadPopular()
     }
 
     const loadUpdatedLinks = async() =>{
@@ -56,13 +96,6 @@ const Links = ({query,category,links,totalLinks,linksLimit,linkSkip}) =>{
             setSize(response.data.links.length)
             setSkip(toSkip)
         }
-        // const loadMoreButton = () =>{
-        //     return(
-        //         size > 0 && size >= limit && (
-        //             <button onClick={loadMore} className="btn btn-outline-primary btn-lg">Load More</button>
-        //         )
-        //     )
-        // }
 
     return (<Layout>
         <div className="row">
@@ -75,7 +108,6 @@ const Links = ({query,category,links,totalLinks,linksLimit,linkSkip}) =>{
             </div>
         </div>
         <br/>
-        {/* <div className="text-center pt-4 pb-5">{loadMoreButton()}</div> */}
         <InfiniteScroll
             pageStart={0}
             loadMore={loadMore}
@@ -91,7 +123,9 @@ const Links = ({query,category,links,totalLinks,linksLimit,linkSkip}) =>{
             </div>
             <div className="col-md-4">
                 <h2 className="lead">Most popular in {category.name}</h2>
-                <p>show popular links</p>
+                <div className="p-3">
+                    {listOfPopularLinks()}
+                </div>
             </div>
         </div>
         </InfiniteScroll>
